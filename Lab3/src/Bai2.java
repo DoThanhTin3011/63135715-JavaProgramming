e122ab554bba92cfb917316cbecb505bde9eefaa
DoThanhTin123 <tin.dt.63cntt@ntu.edu.vn>
2024-03-01 07:41:48 +0700

    Lab3,bai2

--- Lab3/src/Bai2.java
+++ Lab3/src/Bai2.java
@@ -3,7 +3,13 @@ public class Bai2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		for(int i=1; i<= 9; i++) {
+			for(int j=1; j<= 10; j++) {
+				int ketqua = i*j;
+				System.out.printf("%d x %d = %d\n", i,j,ketqua);
+			}
+			System.out.println();
+		}
 	}
 
 }
